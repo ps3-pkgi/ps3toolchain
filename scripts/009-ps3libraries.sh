45adc8745d62b1f2d9c2b0680b62e5b50b720c21
Ayayoku Togetsu <130299352+dpkg123@users.noreply.github.com>
2024-12-11 20:15:23 +0800

    Update 009-ps3libraries.sh

--- scripts/009-ps3libraries.sh
+++ scripts/009-ps3libraries.sh
@@ -6,7 +6,7 @@ PS3LIBRARIES="ps3libraries"
 if [ ! -d ${PS3LIBRARIES} ]; then
 
     ## Download the source code.
-     aria2c https://github.com/humbertodias/$PS3LIBRARIES/tarball/psl1ght-2.30.1 -o $PS3LIBRARIES.tar.gz
+     aria2c https://github.com/bucanero/$PS3LIBRARIES/tarball/psl1ght-2.30.1 -o $PS3LIBRARIES.tar.gz
 
     ## Unpack the source code.
     rm -Rf $PS3LIBRARIES && mkdir $PS3LIBRARIES && tar --strip-components=1 --directory=$PS3LIBRARIES -xzf $PS3LIBRARIES.tar.gz
