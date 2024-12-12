4672d4d7980ebd6251eeae71eb42b13e17d2c317
Ayayoku Togetsu <130299352+dpkg123@users.noreply.github.com>
2024-12-12 10:39:59 +0800

    更新 009-ps3libraries.sh

--- scripts/009-ps3libraries.sh
+++ scripts/009-ps3libraries.sh
@@ -8,9 +8,6 @@ if [ ! -d ${PS3LIBRARIES} ]; then
     ## Download the source code.
     git clone https://github.com/bucanero/ps3libraries --depth=1
 
-    ## Unpack the source code.
-    rm -Rf $PS3LIBRARIES && mkdir $PS3LIBRARIES && unzip $PS3LIBRARIES.zip
-
 fi
 cd $PS3LIBRARIES
 
