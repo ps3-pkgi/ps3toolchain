919756ba0a819371afd45feacf4ee4f0c4c477f9
Ayayoku Togetsu <130299352+dpkg123@users.noreply.github.com>
2024-12-12 10:39:31 +0800

    更新 009-ps3libraries.sh

--- scripts/009-ps3libraries.sh
+++ scripts/009-ps3libraries.sh
@@ -6,13 +6,12 @@ PS3LIBRARIES="ps3libraries"
 if [ ! -d ${PS3LIBRARIES} ]; then
 
     ## Download the source code.
-     aria2c https://github.com/bucanero/ps3libraries/archive/refs/heads/master.zip -o $PS3LIBRARIES.zip
+    git clone https://github.com/bucanero/ps3libraries --depth=1
 
     ## Unpack the source code.
     rm -Rf $PS3LIBRARIES && mkdir $PS3LIBRARIES && unzip $PS3LIBRARIES.zip
 
 fi
-mv ps3libraries-master ps3libraries
 cd $PS3LIBRARIES
 
 ## Compile and install.
