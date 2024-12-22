ba03cd481664334cd20542ee9824173ea2bc1bf4
dpkg123 <test1@qw5d.onmicrosoft.com>
2024-12-22 14:23:03 +0800

    fiz

--- arm64.sh
+++ arm64.sh
@@ -6,7 +6,7 @@ sudo apt install --no-install-recommends -y curl xz-utils git aria2 \
   libseccomp-dev \
   libcap-dev \
   libc6-dev \
-  binutils qemu-user-static debootstrap
+  binutils qemu-user-static
 
 git clone https://github.com/moe-hacker/ruri
 cd ruri
@@ -19,13 +19,15 @@ cd ..
 #ROOTFS_URL=$(curl -s -L "$BASE_URL/latest-releases.yaml" | grep "alpine-minirootfs" | grep "aarch64.tar.gz" | head -n 1 | awk '{print $2}')
 #FULL_URL="$BASE_URL/$ROOTFS_URL"
 #wget "$FULL_URL"
-#mkdir aarch64
+mkdir aarch64
 #tar -xvf "$ROOTFS_URL" -C aarch64
-sudo debootstrap --arch=arm64 --variant=minbase bookworm aarch64
+#sudo debootstrap --arch=arm64 --variant=minbase bookworm aarch64
+aria2c https://github.com/2cd/debian-museum/releases/download/12/12_bookworm_arm64.tar.zst -o root.tar.zst
+tar -xf root.tar.zst -C aarch64
 rm -rf -v scripts/009-ps3libraries.sh
 sudo cp -rv depends patches scripts aarch64/
 sudo cp -v toolchain.sh aarch64/build.sh
 sudo chmod +x -v aarch64/build.sh
 sudo rm aarch64/etc/resolv.conf
-sudo echo nameserver 1.1.1.1 >aarch64/etc/resolv.conf
+sudo echo nameserver 8.8.8.8 >aarch64/etc/resolv.conf
 sudo ./ruri/ruri -a aarch64 -q /usr/bin/qemu-aarch64-static ./aarch64 /bin/sh /build.sh
